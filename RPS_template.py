8b456d0075f183bb370a0d4d05d3d190db189f14
“luca-saccilotto” <luca.sacc@outlook.com>
2023-02-13 23:36:12 +0100

    Updates files

--- RPS_template.py
+++ RPS_template.py
@@ -3,7 +3,7 @@ from keras.models import load_model
 import numpy as np
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+data = np.ndarray(shape = (1, 224, 224, 3), dtype = np.float32)
 
 while True: 
     ret, frame = cap.read()
@@ -20,5 +20,6 @@ while True:
             
 # After the loop release the cap object
 cap.release()
+
 # Destroy all the windows
 cv2.destroyAllWindows()
