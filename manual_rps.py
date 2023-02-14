d68eb2940a94357dc25a90a0cf17c1012e513836
“luca-saccilotto” <luca.sacc@outlook.com>
2023-02-14 10:59:29 +0100

    Updates "Task 2"

--- manual_rps.py
+++ manual_rps.py
@@ -11,19 +11,19 @@ def get_user_choice():
 
 def get_winner(computer_choice, user_choice):
     if computer_choice == user_choice:
-        return("It's a tie!")
+        print("It's a tie!")
     if computer_choice == "Rock":
         if user_choice == "Scissors":
-            return("You lost")
+            print("You lost")
         else:
-            return("You won!")
+            print("You won!")
     elif computer_choice == "Paper":
         if user_choice == "Rock":
-            return("You lost")
+            print("You lost")
         else:
-            return("You won!")
+            print("You won!")
     elif computer_choice == "Scissors":
         if user_choice == "Paper":
-            return("You lost")
+            print("You lost")
         else:
-            return("You won!")
+            print("You won!")
